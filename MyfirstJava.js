8001f6c09038e044bdfb4cace729c1c487a804c2
JeobenAldous <jeobenaldous@gmail.com>
2023-04-12 10:20:57 +0800

    New Function

--- MyfirstJava.js
+++ MyfirstJava.js
@@ -1,15 +1,12 @@
-var ourArray = [];
-
-for (var i = 0; i < 10; i += 3) {
-    ourArray.push(i);
-}
-
-console.log(ourArray)
-
-
-var Myarray = [];
-
-for (var i = 1; i < 10; i += 2){
- Myarray.push(i);
-}
- console.log(Myarray);
+function sinking(participant, rounds){
+    rounds = [4,2,5,3,2];{
+    result = (Math.trunc(participant/rounds[0])*rounds[0])
+    result1 = (Math.trunc(result/rounds[1])*rounds[1])
+    result2 = (Math.trunc(result1/rounds[2])*rounds[2])
+    result3 = (Math.trunc(result2/rounds[3])*rounds[3])
+        exp = (Math.trunc(result3/rounds[4])*rounds[4])} 
+      console.log("participants:"+ participant);
+      console.log("rounds: "+ rounds);
+      console.log("participant left: " + exp);
+    }
+    sinking(11, );
