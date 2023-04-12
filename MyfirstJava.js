654970598aeaca8ef4de47101d40621c74b9edfd
JeobenAldous <jeobenaldous@gmail.com>
2023-04-12 09:13:39 +0800

    First commit

---
+++ MyfirstJava.js
@@ -0,0 +1,15 @@
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 3) {
+    ourArray.push(i);
+}
+
+console.log(ourArray)
+
+
+var Myarray = [];
+
+for (var i = 1; i < 10; i += 2){
+ Myarray.push(i);
+}
+ console.log(Myarray);
